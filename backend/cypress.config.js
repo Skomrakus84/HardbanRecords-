145981227400f215c097c170df545bf2f12501e8
Skomrakus <109104106+Skomrakus84@users.noreply.github.com>
2025-08-30 05:47:15 +0200

    poprawki testowe

---
+++ backend/cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projectId: 'dxgyr7',
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <div>HardbanRecords Lab</div>;
+}
